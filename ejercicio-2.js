3e03d7a17f7e0b2a9651df7c2dfadf22266acdf1
Angie Liceth <liceth5262@gmail.com>
2024-05-30 17:59:14 -0500

    realizado el ejercicio-2

---
+++ ejercicio-2.js
@@ -0,0 +1,18 @@
+let a = 4;
+let b = 7;
+let c = 2 * b;
+let x = a;
+let y = 3 * x;
+let u = 6;
+let w = 10;
+let z = 2 * w;
+
+let resultadoA = a / (b * c);
+let resultadoB = a ** 2 + b ** 92;
+let resultadoC = (x + y) / (u / 1 + w / a );
+let resultadoD = (x / y) * (z + w);
+
+console.log(resultadoA);
+console.log(resultadoB);
+console.log(resultadoC);
+console.log(resultadoD);
