376b84d91f2a0abe4ee3d9f5bb535e658857cf52
Angie Liceth <liceth5262@gmail.com>
2024-05-30 18:49:11 -0500

    realizado el ejercicio-3

---
+++ ejercicio-3.js
@@ -0,0 +1,49 @@
+let p1 = true;
+let p2 = true;
+let p3 = false;
+let p4 = false;
+
+let q1 = true;
+let q2 = false;
+let q3 = true;
+let q4 = false;
+
+console.log("NOT P");
+
+console.log(!p1);
+console.log(!p2);
+console.log(!p3);
+console.log(!p4);
+
+console.log("NOT P AND Q");
+
+
+console.log(!p1 && q1);
+console.log(!p2 && q2);
+console.log(!p3 && q3);
+console.log(!p4 && q4);
+
+
+console.log("Aquí empieza el ejercicio B");
+
+console.log("NOT P");
+
+console.log(!p1);
+console.log(!p2);
+console.log(!p3);
+console.log(!p4);
+
+console.log("NOT Q");
+
+console.log(!q1);
+console.log(!q2);
+console.log(!q3);
+console.log(!q4);
+
+
+console.log("NOT P AND NOT Q");
+
+console.log(!p1 && !q1);
+console.log(!p2 && !q2);
+console.log(!p3 && !q3);
+console.log(!p4 && !q4);
